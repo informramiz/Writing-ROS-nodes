1a61892866d237cc93bb4a38e37eb5a98716e775
Ramiz Raja <informramiz@gmail.com>
2017-09-14 17:53:23 -0700

    feat: Create a service GoToPosition.srv and add dependencies

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/student/Writing-ROS-nodes/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/student/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/student/catkin_ws/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/student/Writing-ROS-nodes/catkin_ws/devel;/home/student/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/student/Writing-ROS-nodes/catkin_ws/devel;/home/student/catkin_ws/devel;/opt/ros/kinetic" != "" else []
